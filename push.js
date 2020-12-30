cc07c56b44220735d0ad55ac37199bcba593328d
govtz <76772437+govtz@users.noreply.github.com>
2020-12-30 17:40:57 +0800

    123

---
+++ push.js
@@ -0,0 +1,6 @@
+var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?e58cf9463b018cdc8d37b5a837e99f7d";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s);})();(function(){var bp=document.createElement('script');var curProtocol=window.location.protocol.split(':')[0];if(curProtocol==='https'){bp.src='https://zz.bdstatic.com/linksubmit/push.js';}
+else{bp.src='http://push.zhanzhang.baidu.com/push.js';}
+var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(bp,s);})();
+var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
